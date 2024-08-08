4cdff644736485b48e8639e236a042be6fb5f5f0
Andrei <60008503+ysnisokol@users.noreply.github.com>
2024-08-08 15:38:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    n = String(n);
+    n = n.split("");
+    n = n.reverse();
+    n = n.join("");
+    n = Number(n); 
+    return n;
 }
